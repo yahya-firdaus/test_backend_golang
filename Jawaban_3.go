a328f84835ea5ab8741e6d24c5ecd81d82ccbddd
yahya-firdaus <77067203+yahya-firdaus@users.noreply.github.com>
2024-05-22 12:24:53 +0700

    Update Jawaban_3.go

--- Jawaban_3.go
+++ Jawaban_3.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Fugsi rekursif untuk highest palindrome
-func highestPalindromeRecursive(s []rune, left int, right int, k int) []rune {
+func mencariPalindromTertinggiRecursive(s []rune, left int, right int, k int) []rune {
 	// Jika sebelah kiri lebih besar daripada kanan, maka akan return string
 	if left > right {
 		return s
@@ -14,7 +14,7 @@ func highestPalindromeRecursive(s []rune, left int, right int, k int) []rune {
 
 	// Jika char sudah sama
 	if s[left] == s[right] {
-		return highestPalindromeRecursive(s, left+1, right-1, k)
+		return mencariPalindromTertinggiRecursive(s, left+1, right-1, k)
 	}
 
 	// Pengecekan jika diharuskan me replace char
@@ -24,7 +24,7 @@ func highestPalindromeRecursive(s []rune, left int, right int, k int) []rune {
 		} else {
 			s[left] = s[right]
 		}
-		return highestPalindromeRecursive(s, left+1, right-1, k-1)
+		return mencariPalindromTertinggiRecursive(s, left+1, right-1, k-1)
 	}
 
 	// Jika tidak ada yang perlu untuk di replace dan char tidak sama maka mengembalikan nil
@@ -32,13 +32,13 @@ func highestPalindromeRecursive(s []rune, left int, right int, k int) []rune {
 }
 
 // Fungsi untuk maximize nilai palindrom
-func maximizePalindrome(s []rune, left int, right int, k int) []rune {
+func maxPalindrom(s []rune, left int, right int, k int) []rune {
 	if left > right {
 		return s
 	}
 
 	if s[left] == s[right] {
-		return maximizePalindrome(s, left+1, right-1, k)
+		return maxPalindrom(s, left+1, right-1, k)
 	}
 
 	if k > 1 {
@@ -50,24 +50,24 @@ func maximizePalindrome(s []rune, left int, right int, k int) []rune {
 			s[right] = '9'
 			k--
 		}
-		return maximizePalindrome(s, left+1, right-1, k)
+		return maxPalindrom(s, left+1, right-1, k)
 	}
 
 	return s
 }
 
 // Fungsi untuk menentukan palindrom yang tertinggi
-func highestPalindrome(s string, k int) string {
+func prosesPalindrome(s string, k int) string {
 	// Mengubah string menjadi rune
 	rs := []rune(s)
 	n := len(rs)
 
-	rs = highestPalindromeRecursive(rs, 0, n-1, k)
+	rs = mencariPalindromTertinggiRecursive(rs, 0, n-1, k)
 	if rs == nil {
 		return "-1"
 	}
 
-	rs = maximizePalindrome(rs, 0, n-1, k)
+	rs = maxPalindrom(rs, 0, n-1, k)
 
 	return string(rs)
 }
@@ -76,13 +76,13 @@ func main() {
 	// Contoh inputan
 	input1 := "3943"
 	k1 := 1
-	fmt.Printf("Input: %s, k: %d\nOutput: %s\n", input1, k1, highestPalindrome(input1, k1))
+	fmt.Printf("Input: %s, k: %d\nOutput: %s\n", input1, k1, prosesPalindrome(input1, k1))
 
 	input2 := "932239"
 	k2 := 2
-	fmt.Printf("Input: %s, k: %d\nOutput: %s\n", input2, k2, highestPalindrome(input2, k2))
+	fmt.Printf("Input: %s, k: %d\nOutput: %s\n", input2, k2, prosesPalindrome(input2, k2))
 
 	input3 := "12321"
 	k3 := 1
-	fmt.Printf("Input: %s, k: %d\nOutput: %s\n", input3, k3, highestPalindrome(input3, k3))
+	fmt.Printf("Input: %s, k: %d\nOutput: %s\n", input3, k3, prosesPalindrome(input3, k3))
 }
